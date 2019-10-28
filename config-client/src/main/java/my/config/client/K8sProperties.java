d11950ca6a2670a4d364e559adffb358511d2983
Kiran <kirodod@gmail.com>
2019-10-28 23:47:17 +0530

    updates

--- config-client/src/main/java/my/config/client/K8sProperties.java
+++ config-client/src/main/java/my/config/client/K8sProperties.java
@@ -1,18 +1,3 @@
-/*
- * Copyright 2019 Google, Inc.
- *
- * Licensed under the Apache License, Version 2.0 (the "License")
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package my.config.client;
 
 import lombok.Data;
@@ -26,13 +11,12 @@ import java.util.List;
 @Component
 @ConfigurationProperties(prefix = "kubernetes")
 public class K8sProperties {
-  private static final int DEFAULT_CACHE_THREADS = 1;
   private List<ManagedAccount> accounts = new ArrayList<>();
 
   @Data
   public static class ManagedAccount {
     private String name;
-    private ProviderVersion providerVersion = ProviderVersion.v1;
+    private ProviderVersion providerVersion;
     private String environment;
     private String accountType;
     private String context;
@@ -49,16 +33,12 @@ public class K8sProperties {
     private List<String> namespaces = new ArrayList<>();
     private List<String> omitNamespaces = new ArrayList<>();
     private String skin;
-    private int cacheThreads = DEFAULT_CACHE_THREADS;
-//    private List<LinkedDockerRegistryConfiguration> dockerRegistries;
+    private int cacheThreads;
     private List<String> requiredGroupMembership = new ArrayList<>();
-//    private Permissions.Builder permissions = new Permissions.Builder();
     private String namingStrategy = "kubernetesAnnotations";
     private boolean debug = false;
     private boolean metrics = true;
     private boolean checkPermissionsOnStartup = true;
-//    private List<CustomKubernetesResource> customResources = new ArrayList<>();
-//    private List<KubernetesCachingPolicy> cachingPolicies = new ArrayList<>();
     private List<String> kinds = new ArrayList<>();
     private List<String> omitKinds = new ArrayList<>();
     private boolean onlySpinnakerManaged = false;
